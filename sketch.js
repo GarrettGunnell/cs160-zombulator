682009373215c837c95f154225cf6b283cc11fd6
Garrett Gunnell <garrett99@bendbroadband.com>
2017-10-03 16:08:42 -0700

    Draws circles with mouse

--- sketch.js
+++ sketch.js
@@ -1,10 +1,15 @@
 // Zombulator by Garrett Gunnell
 
 function setup() {
-
+	createCanvas(1000, 1000);
 }
 
 function draw() {
-
+	if (mouseIsPressed) {
+		fill(51, 0, 51);
+	} else {
+		fill(255, 153, 255);
+	}
+	ellipse(mouseX, mouseY, 80, 80);
 }
 
