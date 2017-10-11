7391962f60772fffc9654eeffa7cb9ddb8cb59f4
Garrett Gunnell <garrett99@bendbroadband.com>
2017-10-10 17:10:59 -0700

    Zombie and human bouncy balls.

--- sketch.js
+++ sketch.js
@@ -1,62 +1,48 @@
 // Zombulator by Garrett Gunnell
 
 
-var zombieX = 0
-var zombie2X = 50
-var zombieY = 0
-var zombieReverse = 1920
-
+var zombieX = 0;
+var zombieY = 100;
+var zombieV = 0;
+var zombieA = 0.3;
+var zombieDamping = -0.6;
+var zombieSize = 80
+var zombieColor;
+var humanY = 175;
+var humanX = 175;
+var humanV = 0;
+var humanA = 0.2;
+var humanDamping = -0.7;
+var humanSize = 50
+var humanColor;
+var backgroundColor;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	backgroundColor = color(255, 198, 224);
+	humanColor = color(117, 160, 216);
+	zombieColor = color(43, 206, 127);
 }
 
 function draw() {
-	var randomnum = Math.floor((Math.random() * 81) + 50);
-	var randomnum2 = Math.floor(Math.random() * 151);
-	var randomSize = Math.floor((Math.random() * 50) + 10)
-	var randomcolor1 = Math.floor(Math.random() * 256);
-	var randomcolor2 = Math.floor(Math.random() * 256);
-	var randomcolor3 = Math.floor(Math.random() * 256);
-	var randompos1 = Math.floor(Math.random() * 1921);
-	var randompos2 = Math.floor(Math.random() * 1081);
-	background(255, 255, 255);
+	background(backgroundColor);
 	noStroke();
-	fill(255,128,255);
-	ellipse(zombieX, 50, 80, 80);
-	ellipse(zombie2X, 150, 80, 80);
-	//zombieY = zombieY + 1;
-
-	//text(windowWidth, 100, 100)
-
-	if (zombieX >= 1920) {
-		if (zombieReverse <= 1920 && zombieReverse > 0) {
-			fill(255,255,255);
-			ellipse(zombieX, 50, 90, 90);
-			fill(255,128,255);
-			ellipse(zombieReverse, 50, 80, 80);
-			zombieReverse = zombieReverse - 5;
-		} else if (zombieReverse <= 0) {
-			zombieX = 0;
-			zombieReverse = 1920;
-		}
-	} else {
-		zombieX = zombieX + 5;
+	fill(zombieColor);
+	ellipse(windowWidth / 3, zombieY, zombieSize, zombieSize)
+	zombieY += zombieV;
+	zombieV += zombieA;
+	if (zombieY + (zombieSize / 2) >= windowHeight) {
+		zombieY = windowHeight - (zombieSize / 2);
+		zombieV *= zombieDamping;
 	}
 
-	if (zombie2X >=windowWidth) {
-		zombie2X = 0
-	} else {
-		zombie2X = zombie2X + 1
+	fill(humanColor);
+	ellipse(humanX, 250, humanSize, humanSize)
+	humanX += humanV;
+	humanV += humanA;
+	if (humanX + (humanSize / 2) >= windowWidth) {
+		humanX = windowWidth - (humanSize / 2);
+		humanV *= humanDamping;
 	}
-
-	//fill(randomcolor1, 0,0);
-	//ellipse(randompos1, randompos2, randomnum, randomnum);
-	//fill(0, 0, 0);
-	//textSize(randomSize);
-	//text("A", zombieX, 12);
-
-	//fill(223,0,0);
-	//ellipse(mouseX, mouseY, 80, 80);
 }
 
