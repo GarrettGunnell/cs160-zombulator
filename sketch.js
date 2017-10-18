d03d529b9884970bff76400b0e30d606d5acc8b8
Garrett Gunnell <garrett99@bendbroadband.com>
2017-10-18 12:31:59 -0700

    Zombie and Human Arrays

--- sketch.js
+++ sketch.js
@@ -1,118 +1,98 @@
-// Zombulator by Garrett Gunnell
+ // Zombulator by Garrett Gunnell
 
 
-var zombieY;
-var zombieX;
-var zombieV = 0;
-var zombieA = 0.8;
-var zombieDamping = -0.2;
-var zombieSize = 80;
-var zombieColor;
+var zombieYs;
+var zombieXs;
+var zombieSizes;
+var zombieColors;
 
-var humanY;
-var humanX;
-var humanV = 0;
-var humanVx = 0;
-var humanA = 0.3;
-var humanAx = 0.07;
-var humanDamping = -0.7;
-var humanSize;
-var humanColor;
+var humanYs;
+var humanXs;
+var humanSizes;
+var humanColors;
 
-var img;
-var imgX = 1000;
-var imgY = 100;
-var imgV = 0;
-var imgA = 0.3;
-var imgDamping = -0.4;
-var imgSize = 150;
+var circleX;
+var circleY;
 
 var backgroundColor;
 
 const MIN_SIZE = 25;
-const MAX_SIZE = 150;
-
-function preload() {
-	img = loadImage("https://i.imgur.com/I5mZrzP.jpg")
-}
+const MAX_SIZE = 80;
+const NUMBER_OF_HUMANS = 100;
+const NUMBER_OF_ZOMBIES = 100;
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	backgroundColor = color(255, 250, 244);
-	initializeZombie();
-	initializeHuman();
+	initializeZombies();
+	initializeHumans();
+	circleX = windowWidth / 2;
+	circleY = windowHeight / 2;
 }
 
 function draw() {
 	background(backgroundColor);
   	noStroke();
-  	//image(img, imgX, imgY, imgSize, imgSize);
 
-  	drawZombie();
-  	//moveZombie();
-  	drawHuman();
-  	//moveHuman();
-  	zombieY += 1
-  	humanY -= 1
+  	drawZombies();
+  	drawHumans();
+  	moveZombies();
+  	moveHumans();
 
 }
 
-function moveImg() {
-	imgY += imgV;
-	imgV += imgA;
-	if (imgY + imgSize >= windowHeight) {
-		imgY = windowHeight - imgSize;
-		imgV *= imgDamping;
+function initializeZombies() {
+	zombieXs = [];
+	zombieYs = [];
+	zombieSizes = [];
+	zombieColors = [];
+	for (var i = 0; i < NUMBER_OF_ZOMBIES; ++i) {
+		zombieXs[i] = random(100, windowWidth - 100);
+		zombieYs[i] = random(50, 150);
+		zombieSizes[i] = random(MIN_SIZE,MAX_SIZE);
+		zombieColors[i] = color(random(100,200), 255, random(100,200), 150);
 	}
 }
 
-function initializeZombie() {
-	zombieX = random(100, windowWidth - 100);
-	zombieY = random(100, 200);
-	zombieSize = random(MIN_SIZE, MAX_SIZE);
-	zombieColor = color(random(100, 200), 255, 0, random(100, 200));
+function drawZombies() {
+	for (var i = 0;i < NUMBER_OF_ZOMBIES; ++i) {
+		fill(zombieColors[i]);
+		ellipse(zombieXs[i], zombieYs[i], zombieSizes[i], zombieSizes[i]);
+	}
 }
 
-function drawZombie() {
-	fill(zombieColor);
-  	ellipse(zombieX, zombieY, zombieSize, zombieSize);
+function moveZombies() {
+	for (var i = 0;i < NUMBER_OF_ZOMBIES; ++i) {
+		zombieYs[i] += 0.5;
+		zombieXs[i] += random(-1,1);
+	}
 }
 
-function moveZombie() {
-	zombieY += zombieV;
-  	zombieV += zombieA;
-  	if (zombieY + (zombieSize) >= windowHeight) {
-    	zombieY = windowHeight - (zombieSize);
-    	zombieV *= zombieDamping;
-  	}
-}
+function initializeHumans() {
+	humanXs = []
+	humanYs = []
+	humanColors = []
+	humanSizes = []
 
-function initializeHuman() {
-	humanX = random(100, windowWidth - 100);
-	humanY = random(windowHeight - 200, windowHeight - 100);
-	humanSize = random(MIN_SIZE, MAX_SIZE);
-	humanColor = color(random(75,200),random(75,200),255, 150);
+	for (var i = 0;i < NUMBER_OF_HUMANS; ++i) {
+		humanXs[i] = random(100, windowWidth - 100);
+		humanYs[i] = random(windowHeight - 150, windowHeight - 50);
+		humanColors[i] = color(random(100,200), 200, 255, 150);
+		humanSizes[i] = random(MIN_SIZE, MAX_SIZE);
+	}
 
 }
 
-function drawHuman() {
-	fill(humanColor);
-  	ellipse(humanX, humanY, humanSize, humanSize);
+function drawHumans() {
+	for (var i = 0;i < NUMBER_OF_HUMANS; ++i) {
+		fill(humanColors[i]);
+		ellipse(humanXs[i], humanYs[i], humanSizes[i], humanSizes[i]);
+	}
 }
 
-function moveHuman() {
-	humanY += humanV;
-	humanX += humanVx;
-  	humanV += humanA;
-  	humanVx += humanAx;
-  	if (humanY + (humanSize / 2) >= windowHeight) {
-    	humanY = windowHeight - (humanSize / 2);
-    	humanV *= humanDamping;
-  	}
-  	if (humanX - (humanSize / 2) >= windowWidth) {
-  		humanX = 0
-  		humanY = 0
-  		humanV = 0
-  		humanVx = 0
-  	}
+function moveHumans() {
+	for (var i = 0;i < NUMBER_OF_HUMANS; ++i) {
+		humanYs[i] -= 0.5;
+		humanXs[i] += random(-1,1);
+	}
 }
