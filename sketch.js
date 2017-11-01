eea73e441583ebcc102ac636fca91eb36903f216
Garrett Gunnell <garrett99@bendbroadband.com>
2017-11-01 09:09:14 -0700

    Draw and Move functions now inside the object

--- sketch.js
+++ sketch.js
@@ -4,9 +4,14 @@ var zombies;
 var humans;
 var backgroundColor;
 const MIN_SIZE = 25;
-const MAX_SIZE = 50;
+const MAX_SIZE = 60;
 const NUMBER_OF_HUMANS = 100;
 const NUMBER_OF_ZOMBIES = 100;
+
+
+function preload() {
+	zombieBackground = loadImage("https://i.imgur.com/Own5Doo.png");
+}
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
@@ -16,13 +21,13 @@ function setup() {
 }
 
 function draw() {
-	background(255);
+	background(zombieBackground);
   	noStroke();
 
   	fill(0);
   	textFont("Arial", 10);
-  	text("Zombies: " + NUMBER_OF_ZOMBIES, windowWidth / 2, 25);
-  	text("Humans: " + NUMBER_OF_HUMANS, windowWidth / 2, windowHeight - 25);
+  	//text("Zombies: " + NUMBER_OF_ZOMBIES, width / 2, 25);
+  	//text("Humans: " + NUMBER_OF_HUMANS, width / 2, height - 25);
 
   	drawZombies();
   	drawHumans();
@@ -35,79 +40,72 @@ function initializeZombies() {
 	zombies = [];
 
 	for (var i = 0; i < NUMBER_OF_ZOMBIES; ++i) {
-		initializeZombie(i);
+		zombies[i] = initializeZombie();
 	}
 }
 
-function initializeZombie(index) {
-	zombies[index] = {
-		position: createVector(random(100, windowWidth - 100), random(50, 150)),
+function initializeZombie() {
+	return {
+		position: createVector(random(100, width - 100), random(50, 150)),
 		size: random(MIN_SIZE,MAX_SIZE),
 		color: color(random(100,200), 255, random(100,200), 200),
 		speed: random(0.5,1),
+		draw: function() {
+			fill(this.color);
+			ellipse(this.position.x, this.position.y, this.size, this.size);	
+		},
+		move: function() {
+			this.position.add(0, this.speed);
+			this.position.add(random(-1,1), random(-1,1));
+		},
 	};
 }
 
 function drawZombies() {
 	for (var i = 0;i < NUMBER_OF_ZOMBIES; ++i) {
-		drawZombie(zombies[i]);
+		zombies[i].draw();
 	}
-}
-
-function drawZombie(zombie) {
-	fill(zombie.color);
-	ellipse(zombie.position.x, zombie.position.y, zombie.size, zombie.size);	
 }
 
 function moveZombies() {
 	for (var i = 0;i < NUMBER_OF_ZOMBIES; ++i) {
-		moveZombie(zombies[i]);
+		zombies[i].move();
 	}
-}
-
-function moveZombie(zombie) {
-	zombie.position.add(0, zombie.speed);
-	zombie.position.add(random(-1,1), random(-1,1));
 }
 
 function initializeHumans() {
 	humans = [];
 
 	for (var i = 0;i < NUMBER_OF_HUMANS; ++i) {
-		initializeHuman(i);
+		humans[i] = initializeHuman();
 	}
 }
 
-function initializeHuman(index) {
-	humans[index] = {
-		position: createVector(random(100, windowWidth - 100), random(windowHeight - 150, windowHeight - 50)),
+function initializeHuman() {
+	return {
+		position: createVector(random(100, width - 100), random(height - 150, height - 50)),
 		color: color(random(100,200), 200, 255, 200),
 		size: random(MIN_SIZE, MAX_SIZE),
 		speed: random(0.5,1),
+		draw: function() {
+			fill(this.color);
+			ellipse(this.position.x, this.position.y, this.size, this.size);
+		},
+		move: function() {
+			this.position.sub(0, this.speed);
+			this.position.add(random(-1,1), random(-1, 1));
+		},
 	}
 }
 
 function drawHumans() {
 	for (var i = 0;i < NUMBER_OF_HUMANS; ++i) {
-		drawHuman(humans[i]);
+		humans[i].draw();
 	}
-}
-
-function drawHuman(human) {
-		fill(human.color);
-		ellipse(human.position.x, human.position.y, human.size, human.size);	
 }
 
 function moveHumans() {
 	for (var i = 0;i < NUMBER_OF_HUMANS; ++i) {
-		moveHuman(humans[i]);
+		humans[i].move();
 	}
 }	
-
-function moveHuman(human) {
-	human.position.sub(0, human.speed);
-	human.position.add(random(-1,1), random(-1,1));
-	//human.y -= human.speed;
-	//human.y -= random(-1,1);
-	//human.x += random(-1,1);	
-}
