518eaef06471a6e1b580badf76561c633cba6be3
Garrett Gunnell <garrett99@bendbroadband.com>
2017-11-26 19:02:21 -0800

    Added comments and cleaned up code

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
  // Zombulator by Garrett Gunnell
+ // Humans and zombie objects move towards each other and interact once they collide
 
 var population = [];
 var fightText = [];
@@ -8,10 +9,7 @@ var skull;
 const MIN_SIZE = 20;
 const MAX_SIZE = 40;
 const POPULATION_SIZE = 200;
-const POPULATION_RATIO = 50;
-
-var circle1;
-var circle2;
+const POPULATION_RATIO = 40;
 
 var numFights = 0;
 var zombiePop = 0;
@@ -32,8 +30,6 @@ function draw() {
 	background(img);
 	noStroke();
 
-	//image(skull, width / 2, height / 2, 60, 55);
-
 	drawSkulls();
 	drawPopulationCount();
 	drawPopulation();
@@ -43,14 +39,11 @@ function draw() {
 	//moveFightText();
 }
 
-function initializePopulation() {
+function initializePopulation() { // Creates zombie and human objects and puts them in an array
 	for (var i = 0; i < POPULATION_SIZE; ++i) {
 		x = random(0, 100);
 
-		if (x <= 5) {
-			population[i] = initializeSuperZombie();
-			++zombiePop;
-		} else if (x <= POPULATION_RATIO) {
+		if (x <= POPULATION_RATIO) {
 			population[i] = initializeZombie();
 			++zombiePop;
 		}	else {
@@ -60,7 +53,7 @@ function initializePopulation() {
 	}
 }
 
-function drawPopulationCount() {
+function drawPopulationCount() { // Draws how many humans and zombies there are
 	fill(0);
   textFont("Verdana", 20);
   stroke(0);
@@ -68,20 +61,20 @@ function drawPopulationCount() {
   text("Humans: " + humanPop, width / 2, height - 30);
 }
 
-function drawPopulation() {
+function drawPopulation() { // Utilizes the draw object behavior to draw the objects in the population array
 	noStroke();
 	for (var i = 0; i < POPULATION_SIZE; ++i) {
 		population[i].draw();
 	}
 }
 
-function movePopulation() {
+function movePopulation() { // Utilizes the move object behavior to move the objects in the population array
 	for (var i = 0; i < POPULATION_SIZE; ++i) {
 		population[i].move();
 	}
 }
 
-function initializeZombie() {
+function initializeZombie() { // Zombie object
 	return {
 		position: createVector(random(100, width - 100), random(50, 150)),
 		size: random(MIN_SIZE,MAX_SIZE),
@@ -101,11 +94,7 @@ function initializeZombie() {
 		isTouching: function(defender) {
 			var d = dist(this.position.x, this.position.y, defender.position.x, defender.position.y);
 
-			if (d < (this.size / 2) + (defender.size / 2)) {
-				return true
-			} else {
-				return false
-			}
+			return d < (this.size / 2) + (defender.size / 2)
 		},
 		becomeZombie: function() {
 			return;
@@ -129,56 +118,7 @@ function initializeZombie() {
 	};
 }
 
-function initializeSuperZombie() {
-	return {
-		position: createVector(random(100, width - 100), random(50, 150)),
-		size: 50,
-		color: color(random(100,200), 255, random(100,200), 255),
-		speed: random(0.5,1),
-		isZombie: true, 
-		isHuman: false,
-		infectionChance: 100,
-		draw: function() {
-			fill(this.color);
-			ellipse(this.position.x, this.position.y, this.size, this.size);	
-		},
-		move: function() {
-			this.position.add(0, this.speed);
-			this.position.add(random(-1,1), random(-1,1));
-		},
-		isTouching: function(defender) {
-			var d = dist(this.position.x, this.position.y, defender.position.x, defender.position.y);
-
-			if (d < (this.size / 2) + (defender.size / 2)) {
-				return true
-			} else {
-				return false
-			}
-		},
-		becomeZombie: function() {
-			return;
-		},
-		isDead: function() {
-			this.position = createVector(0, 0);
-			this.color = (0,0,0,0);
-			this.speed = 0;
-			this.isZombie = false;
-			this.isHuman = false;
-			this.draw = function() {
-				return;
-			};
-			this.move = function() {
-				return;
-			};
-			this.isTouching = function() {
-				return;
-			};
-		}
-	};
-}
-
-
-function initializeHuman() {
+function initializeHuman() { // Human object
 	return {
 		position: createVector(random(100, width - 100), random(height - 150, height - 50)),
 		color: color(random(100,200), 200, 255, 200),
@@ -198,11 +138,7 @@ function initializeHuman() {
 		isTouching: function(defender) {
 			var d = dist(this.position.x, this.position.y, defender.position.x, defender.position.y);
 
-			if (d < (this.size / 2) + (defender.size / 2)) {
-				return true
-			} else {
-				return false
-			}
+			return d < (this.size / 2) + (defender.size / 2)
 		},
 		becomeZombie: function() {
 			this.move = function() {
@@ -239,12 +175,11 @@ function initializeHuman() {
 	}
 }
 
-function handleCollisions() {
+function handleCollisions() { // Detects collisions between humans and zombies
 	for (var i = 0; i < POPULATION_SIZE; ++i) {
 		var attacker = population[i];
 		for (var j = i + 1; j < POPULATION_SIZE; ++j) {
 			var defender = population[j];
-			var encounter = random(0, 100);
 
 			handleFights(attacker, defender);
 		}
@@ -252,7 +187,7 @@ function handleCollisions() {
 }
 
 
-function handleFights(attacker, defender) {
+function handleFights(attacker, defender) { // Handles the outcomes of the collisions between humans and zombies
 	var encounter = random(0, 100);
 
 	if (attacker.isTouching(defender) == true && attacker.isZombie == true && defender.isHuman == true && encounter <= attacker.infectionChance) {
@@ -278,7 +213,7 @@ function handleFights(attacker, defender) {
 	}
 }
 
-function initializeSkull(defenderX, defenderY) {
+function initializeSkull(defenderX, defenderY) { // Skull and cross bones object
 	return {
 		position: createVector(defenderX, defenderY),
 		draw: function() {
@@ -287,14 +222,14 @@ function initializeSkull(defenderX, defenderY) {
 	}
 }
 
-function addSkull(defender) {
+function addSkull(defender) { // Adds a skull object to an array
 	defenderX = defender.position.x;
 	defenderY = defender.position.y;
 	skull[numSkulls] = initializeSkull(defenderX, defenderY);
 	numSkulls += 1;
 }
 
-function drawSkulls() {
+function drawSkulls() { // Draws the skulls, showing where a zombie died
 	for (var i = 0; i < numSkulls; ++i) {
 		skull[i].draw();
 	}
